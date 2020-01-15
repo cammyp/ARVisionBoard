4e05cbad0e5806754202f92f1882c6b950e57b0d
Cameron Partee <cp3082@nyu.edu>
2020-01-15 14:24:00 -0500

    in the process of debugging and added features to pinch to zoom and pan to move scnnoes

--- ARVisionBoard/ViewController.swift
+++ ARVisionBoard/ViewController.swift
@@ -9,6 +9,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var nodeArray = [SCNNode]()
+    
+    
     // MARK: Override Functions
     
     override func viewDidLoad() {
@@ -23,6 +26,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin,
         ARSCNDebugOptions.showFeaturePoints]
         sceneView.automaticallyUpdatesLighting = true
+        
+        // add note
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(onClickImageView))
+        //let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(actionPinchGesture))
+        sceneView.addGestureRecognizer(panGesture)
+        //sceneView.addGestureRecognizer(pinchGesture)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,7 +42,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.run(configuration)
         
         // add all functions here
-        
+        addPlaneAtOrigin()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -63,6 +72,52 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         plane.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "rings")
         plane.geometry?.firstMaterial?.isDoubleSided = true;
         plane.position = SCNVector3(0,0,0)
+        plane.name = "rings"
         sceneView.scene.rootNode.addChildNode(plane)
+        nodeArray.append(plane)
     }
+    
+    // builtin touch listener
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        let location = touch.location(in: sceneView)
+        let hitList = sceneView.hitTest(location, options: nil)
+        
+        if let hitObject = hitList.first {
+            let _ = hitObject.node
+            //print("\(node.name ?? "") was pressed")
+            // remove node here
+        }
+    }
+    
+    @objc func onClickImageView(recogizer: UIPanGestureRecognizer) {
+        let translation = recogizer.translation(in: self.view)
+        let hits = sceneView.hitTest(translation, options: nil)
+        //print("enter function")
+        if !hits.isEmpty{
+            //print("hits is not empty")
+            let node = hits.first?.node
+            print("\(node?.name ?? "") was panned")
+            node?.position = SCNVector3(CGFloat((node?.position.x)!) + translation.x,
+                                        CGFloat((node?.position.y)!) + translation.y,
+                                        CGFloat((node?.position.z)!))
+        }
+//        if let view = recogizer.view {
+//            view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y + translation.y)
+//        }
+//        recogizer.setTranslation(CGPoint.zero, in: self.view)
+    }
+       
+       
+//    @objc func actionPinchGesture(recognizer: UIPinchGestureRecognizer) {
+//           if let view = recognizer.view {
+//               view.transform = view.transform.scaledBy(x: recognizer.scale, y: recognizer.scale)
+//               recognizer.scale = 1
+//           }
+//       }
+       
+}
+
+struct PicNode {
+    
 }
